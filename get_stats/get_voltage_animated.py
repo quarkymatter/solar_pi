1685cdd5b0a532c97034345f73fab761b1d28e27
Whitney Osborn <132121881+quarkymatter@users.noreply.github.com>
2023-07-12 14:16:13 -0600

    Update get_voltage_animated.py

--- get_stats/get_voltage_animated.py
+++ get_stats/get_voltage_animated.py
@@ -11,11 +11,14 @@ from threading import Thread
 import matplotlib.animation as animation
 
 
+
+
+
 url = 'http://155.101.22.137:5000'
 refresh_interval = 60  # seconds
 
 battery_voltage_data = []
-solar_voltage_data = []
+#solar_voltage_data = []
 
 
 def fetch_voltage_data():
@@ -26,10 +29,10 @@ def fetch_voltage_data():
             battery_voltage = float(line.split()[1])
             battery_timestamp = datetime.now().strftime('%H:%M')
             battery_voltage_data.append((battery_timestamp, battery_voltage))
-        elif line.startswith('solarshed_solar_volts'):
-            solar_voltage = float(line.split()[1])
-            solar_timestamp = datetime.now().strftime('%H:%M')
-            solar_voltage_data.append((solar_timestamp, solar_voltage))
+        #elif line.startswith('solarshed_solar_volts'):
+            #solar_voltage = float(line.split()[1])
+            #solar_timestamp = datetime.now().strftime('%H:%M')
+            #solar_voltage_data.append((solar_timestamp, solar_voltage))
 
 
 def update_data():
@@ -37,24 +40,25 @@ def update_data():
         fetch_voltage_data()
         time.sleep(refresh_interval)
 
-
-
 def update_graph(i):
     battery_timestamps, battery_voltages = zip(*battery_voltage_data)
-    solar_timestamps, solar_voltages = zip(*solar_voltage_data)
-    
+   # solar_timestamps, solar_voltages = zip(*solar_voltage_data)
+
+    max_voltage = max(battery_voltages)
+    min_voltage = min(battery_voltages)
+    upper_limit = max_voltage + 0.1  # Adding a small margin
+    lower_limit = min_voltage - 0.1  # Subtracting a small margin
+
     ax.clear()
-    ax.plot(battery_timestamps, battery_voltages, label='Battery')
-    ax.plot(solar_timestamps, solar_voltages, label='Solar')
-    
+    ax.plot(battery_timestamps, battery_voltages)
+   # ax.plot(solar_timestamps, solar_voltages, label='Solar')
+    ax.plot(battery_timestamps, [upper_limit] * len(battery_timestamps), 'b--')
+    ax.plot(battery_timestamps, [lower_limit] * len(battery_timestamps), 'g--')
     ax.set_ylabel('Voltage (V)')
     ax.set_xlabel('Time (HH:MM)')
-    ax.set_title('Voltage')
-    ax.legend()
+    ax.set_title('Battery Voltage')
+    #ax.legend()
     plt.xticks(rotation=0)
-    # Add a subtitle to the graph
-    ax.text(0.5, 0.95, f'Battery Temperature: {temperature}°C', transform=ax.transAxes, ha='center')
-
     
     if battery_voltage_data:
         first_battery_timestamp = battery_voltage_data[0][0]
@@ -62,12 +66,15 @@ def update_graph(i):
         ax.set_xticks([first_battery_timestamp, last_battery_timestamp])
         ax.set_xticklabels([first_battery_timestamp, last_battery_timestamp])
     
-    if solar_voltage_data:
-        first_solar_timestamp = solar_voltage_data[0][0]
-        last_solar_timestamp = solar_voltage_data[-1][0]
-        ax.set_xticks([first_solar_timestamp, last_solar_timestamp])
-        ax.set_xticklabels([first_solar_timestamp, last_solar_timestamp])
+   # if solar_voltage_data:
+        #first_solar_timestamp = solar_voltage_data[0][0]
+        #last_solar_timestamp = solar_voltage_data[-1][0]
+        #ax.set_xticks([first_solar_timestamp, last_solar_timestamp])
+        #ax.set_xticklabels([first_solar_timestamp, last_solar_timestamp])
 
+    # Add labels adjacent to the lines
+    ax.annotate(r'$\uparrow$' + f'{upper_limit:.2f} V', (battery_timestamps[-1], upper_limit), xytext=(5, 0), textcoords='offset points', color='blue')
+    ax.annotate(r'$\downarrow$' + f'{lower_limit:.2f} V', (battery_timestamps[-1], lower_limit), xytext=(5, 0), textcoords='offset points', color='green')
 
 
 # Start a thread to update the data
@@ -78,15 +85,15 @@ data_thread.start()
 
 # Temperature data
 
-battery_temp_data = []
-temperature = 0
+#battery_temp_data = []
+#temperature = 0
 
 
 response = requests.get(url)
 data = response.text
-for line in data.split('\n'):
-        if line.startswith('solarshed_battery_temperature_celsius'):
-                temperature = float(line.split()[1])
+#for line in data.split('\n'):
+        #if line.startswith('solarshed_battery_temperature_celsius'):
+                #temperature = float(line.split()[1])
 
 
 # Set the 'ggplot' style
